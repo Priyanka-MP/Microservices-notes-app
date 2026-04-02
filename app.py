e23ee54e2927b6b5d15697f3cd736b4971279c1c
Priyanka-MP <priyanka.mp2731@gmail.com>
2026-04-03 00:55:16 +0530

    deployment adding

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "DevOps Demo app is running as expected!"
 
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
